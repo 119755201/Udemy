4b144db7608f23b04975f1a25f211618354f7601
Corey O Donovan <119755201@umail.ucc.ie>
2020-06-14 17:37:20 +0100

    Completed Calculator Project

---
+++ Calculator/Calculator/Form1.Designer.vb
@@ -0,0 +1,141 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmCalculator
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnAddition = New System.Windows.Forms.Button()
+        Me.btnSubtract = New System.Windows.Forms.Button()
+        Me.btnMultiply = New System.Windows.Forms.Button()
+        Me.txtresultOuput = New System.Windows.Forms.TextBox()
+        Me.txtFirstNumber = New System.Windows.Forms.TextBox()
+        Me.txtSecondNumber = New System.Windows.Forms.TextBox()
+        Me.lblFirstNumber = New System.Windows.Forms.Label()
+        Me.lblSecondNumber = New System.Windows.Forms.Label()
+        Me.lblResult = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnAddition
+        '
+        Me.btnAddition.Location = New System.Drawing.Point(301, 85)
+        Me.btnAddition.Name = "btnAddition"
+        Me.btnAddition.Size = New System.Drawing.Size(153, 41)
+        Me.btnAddition.TabIndex = 0
+        Me.btnAddition.Text = "Addition"
+        Me.btnAddition.UseVisualStyleBackColor = True
+        '
+        'btnSubtract
+        '
+        Me.btnSubtract.Location = New System.Drawing.Point(301, 142)
+        Me.btnSubtract.Name = "btnSubtract"
+        Me.btnSubtract.Size = New System.Drawing.Size(153, 41)
+        Me.btnSubtract.TabIndex = 1
+        Me.btnSubtract.Text = "Subtraction"
+        Me.btnSubtract.UseVisualStyleBackColor = True
+        '
+        'btnMultiply
+        '
+        Me.btnMultiply.Location = New System.Drawing.Point(301, 198)
+        Me.btnMultiply.Name = "btnMultiply"
+        Me.btnMultiply.Size = New System.Drawing.Size(153, 41)
+        Me.btnMultiply.TabIndex = 2
+        Me.btnMultiply.Text = "Multiply"
+        Me.btnMultiply.UseVisualStyleBackColor = True
+        '
+        'txtresultOuput
+        '
+        Me.txtresultOuput.Location = New System.Drawing.Point(183, 312)
+        Me.txtresultOuput.Name = "txtresultOuput"
+        Me.txtresultOuput.Size = New System.Drawing.Size(379, 26)
+        Me.txtresultOuput.TabIndex = 3
+        '
+        'txtFirstNumber
+        '
+        Me.txtFirstNumber.Location = New System.Drawing.Point(37, 142)
+        Me.txtFirstNumber.Name = "txtFirstNumber"
+        Me.txtFirstNumber.Size = New System.Drawing.Size(219, 26)
+        Me.txtFirstNumber.TabIndex = 4
+        '
+        'txtSecondNumber
+        '
+        Me.txtSecondNumber.Location = New System.Drawing.Point(517, 149)
+        Me.txtSecondNumber.Name = "txtSecondNumber"
+        Me.txtSecondNumber.Size = New System.Drawing.Size(219, 26)
+        Me.txtSecondNumber.TabIndex = 5
+        '
+        'lblFirstNumber
+        '
+        Me.lblFirstNumber.AutoSize = True
+        Me.lblFirstNumber.Location = New System.Drawing.Point(33, 95)
+        Me.lblFirstNumber.Name = "lblFirstNumber"
+        Me.lblFirstNumber.Size = New System.Drawing.Size(100, 20)
+        Me.lblFirstNumber.TabIndex = 6
+        Me.lblFirstNumber.Text = "First Number"
+        '
+        'lblSecondNumber
+        '
+        Me.lblSecondNumber.AutoSize = True
+        Me.lblSecondNumber.Location = New System.Drawing.Point(612, 95)
+        Me.lblSecondNumber.Name = "lblSecondNumber"
+        Me.lblSecondNumber.Size = New System.Drawing.Size(124, 20)
+        Me.lblSecondNumber.TabIndex = 7
+        Me.lblSecondNumber.Text = "Second Number"
+        '
+        'lblResult
+        '
+        Me.lblResult.AutoSize = True
+        Me.lblResult.Location = New System.Drawing.Point(349, 277)
+        Me.lblResult.Name = "lblResult"
+        Me.lblResult.Size = New System.Drawing.Size(55, 20)
+        Me.lblResult.TabIndex = 8
+        Me.lblResult.Text = "Result"
+        '
+        'frmCalculator
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lblResult)
+        Me.Controls.Add(Me.lblSecondNumber)
+        Me.Controls.Add(Me.lblFirstNumber)
+        Me.Controls.Add(Me.txtSecondNumber)
+        Me.Controls.Add(Me.txtFirstNumber)
+        Me.Controls.Add(Me.txtresultOuput)
+        Me.Controls.Add(Me.btnMultiply)
+        Me.Controls.Add(Me.btnSubtract)
+        Me.Controls.Add(Me.btnAddition)
+        Me.Name = "frmCalculator"
+        Me.Text = "Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnAddition As Button
+    Friend WithEvents btnSubtract As Button
+    Friend WithEvents btnMultiply As Button
+    Friend WithEvents txtresultOuput As TextBox
+    Friend WithEvents txtFirstNumber As TextBox
+    Friend WithEvents txtSecondNumber As TextBox
+    Friend WithEvents lblFirstNumber As Label
+    Friend WithEvents lblSecondNumber As Label
+    Friend WithEvents lblResult As Label
+End Class
---
+++ Calculator/Calculator/Form1.vb
@@ -0,0 +1,73 @@
+﻿Public Class frmCalculator
+
+    ' Declare variables for later use '
+
+
+    Dim firstNumber, secondNumber, resultOuput As Double
+
+    Private Sub btnSubtract_Click(sender As Object, e As EventArgs) Handles btnSubtract.Click
+
+        ' If the first & second textboxes contain data, that data will be converted to a double integer '
+
+        If txtFirstNumber.Text <> "" And txtSecondNumber.Text <> "" Then
+            firstNumber = CDbl(txtFirstNumber.Text)
+            secondNumber = CDbl(txtSecondNumber.Text)
+            resultOuput = firstNumber - secondNumber
+
+            ' Result is equal to the total of firstNumber - secondNumber '
+
+
+            txtresultOuput.Text = "The result of " & firstNumber & " - " & secondNumber & " = " & resultOuput
+
+            ' In the event that incorrect data is entered e.g non numeric string, a message box will appear '
+
+        Else MessageBox.Show("Please enter both numbers")
+
+        End If
+
+    End Sub
+
+    Private Sub btnMultiply_Click(sender As Object, e As EventArgs) Handles btnMultiply.Click
+
+        If txtFirstNumber.Text <> "" And txtSecondNumber.Text <> "" Then
+            firstNumber = CDbl(txtFirstNumber.Text)
+            secondNumber = CDbl(txtSecondNumber.Text)
+            resultOuput = firstNumber * secondNumber
+            txtresultOuput.Text = "The result of " & firstNumber & " X " & secondNumber & " = " & resultOuput
+
+        Else MessageBox.Show("Please enter both numbers")
+
+        End If
+
+    End Sub
+
+    Private Sub txtFirstNumber_TextChanged(sender As Object, e As EventArgs) Handles txtFirstNumber.TextChanged
+
+        txtresultOuput.Clear()
+
+    End Sub
+
+    Private Sub txtSecondNumber_TextChanged(sender As Object, e As EventArgs) Handles txtSecondNumber.TextChanged
+
+        txtresultOuput.Clear()
+
+    End Sub
+
+    Private Sub frmCalculator_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btnAddition_Click(sender As Object, e As EventArgs) Handles btnAddition.Click
+
+        If txtFirstNumber.Text <> "" And txtSecondNumber.Text <> "" Then
+            firstNumber = CDbl(txtFirstNumber.Text)
+            secondNumber = CDbl(txtSecondNumber.Text)
+            resultOuput = firstNumber + secondNumber
+            txtresultOuput.Text = "The result of " & firstNumber & " + " & secondNumber & " = " & resultOuput
+
+        Else MessageBox.Show("Please enter both numbers")
+
+        End If
+
+    End Sub
+End Class
