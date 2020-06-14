ea49e779b3966f3a183069bd6b03e7a513882037
Corey O Donovan <119755201@umail.ucc.ie>
2020-06-14 19:31:54 +0100

    Created Repair Bill Program using VB.net

---
+++ Repair Bill/Repair Bill/Form1.Designer.vb
@@ -0,0 +1,208 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmRepairBill
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtCustomerName = New System.Windows.Forms.TextBox()
+        Me.txtPartsCost = New System.Windows.Forms.TextBox()
+        Me.txtHoursWorked = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtSuppliesCost = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.lblTotal = New System.Windows.Forms.Label()
+        Me.txtTotalCost = New System.Windows.Forms.TextBox()
+        Me.txtSubTotal = New System.Windows.Forms.TextBox()
+        Me.txtSalesTax = New System.Windows.Forms.TextBox()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtCustomerName
+        '
+        Me.txtCustomerName.Location = New System.Drawing.Point(192, 55)
+        Me.txtCustomerName.Name = "txtCustomerName"
+        Me.txtCustomerName.Size = New System.Drawing.Size(216, 26)
+        Me.txtCustomerName.TabIndex = 0
+        '
+        'txtPartsCost
+        '
+        Me.txtPartsCost.Location = New System.Drawing.Point(192, 119)
+        Me.txtPartsCost.Name = "txtPartsCost"
+        Me.txtPartsCost.Size = New System.Drawing.Size(216, 26)
+        Me.txtPartsCost.TabIndex = 2
+        '
+        'txtHoursWorked
+        '
+        Me.txtHoursWorked.Location = New System.Drawing.Point(192, 87)
+        Me.txtHoursWorked.Name = "txtHoursWorked"
+        Me.txtHoursWorked.Size = New System.Drawing.Size(216, 26)
+        Me.txtHoursWorked.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(54, 90)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(111, 20)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Hours Worked"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(54, 55)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(51, 20)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Name"
+        '
+        'txtSuppliesCost
+        '
+        Me.txtSuppliesCost.Location = New System.Drawing.Point(192, 151)
+        Me.txtSuppliesCost.Name = "txtSuppliesCost"
+        Me.txtSuppliesCost.Size = New System.Drawing.Size(216, 26)
+        Me.txtSuppliesCost.TabIndex = 6
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(54, 125)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(83, 20)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Parts Cost"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(54, 154)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(107, 20)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Supplies Cost"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(48, 240)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(77, 20)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "Sub Total"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(48, 279)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(78, 20)
+        Me.Label6.TabIndex = 10
+        Me.Label6.Text = "Sales Tax"
+        '
+        'lblTotal
+        '
+        Me.lblTotal.AutoSize = True
+        Me.lblTotal.Location = New System.Drawing.Point(48, 314)
+        Me.lblTotal.Name = "lblTotal"
+        Me.lblTotal.Size = New System.Drawing.Size(44, 20)
+        Me.lblTotal.TabIndex = 11
+        Me.lblTotal.Text = "Total"
+        '
+        'txtTotalCost
+        '
+        Me.txtTotalCost.Location = New System.Drawing.Point(192, 304)
+        Me.txtTotalCost.Name = "txtTotalCost"
+        Me.txtTotalCost.ReadOnly = True
+        Me.txtTotalCost.Size = New System.Drawing.Size(216, 26)
+        Me.txtTotalCost.TabIndex = 14
+        '
+        'txtSubTotal
+        '
+        Me.txtSubTotal.Location = New System.Drawing.Point(192, 240)
+        Me.txtSubTotal.Name = "txtSubTotal"
+        Me.txtSubTotal.ReadOnly = True
+        Me.txtSubTotal.Size = New System.Drawing.Size(216, 26)
+        Me.txtSubTotal.TabIndex = 13
+        '
+        'txtSalesTax
+        '
+        Me.txtSalesTax.Location = New System.Drawing.Point(192, 272)
+        Me.txtSalesTax.Name = "txtSalesTax"
+        Me.txtSalesTax.ReadOnly = True
+        Me.txtSalesTax.Size = New System.Drawing.Size(216, 26)
+        Me.txtSalesTax.TabIndex = 12
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(192, 361)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(216, 46)
+        Me.btnCalculate.TabIndex = 15
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'frmRepairBill
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.txtTotalCost)
+        Me.Controls.Add(Me.txtSubTotal)
+        Me.Controls.Add(Me.txtSalesTax)
+        Me.Controls.Add(Me.lblTotal)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtSuppliesCost)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtHoursWorked)
+        Me.Controls.Add(Me.txtPartsCost)
+        Me.Controls.Add(Me.txtCustomerName)
+        Me.Name = "frmRepairBill"
+        Me.Text = "Repair BIll"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtCustomerName As TextBox
+    Friend WithEvents txtPartsCost As TextBox
+    Friend WithEvents txtHoursWorked As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtSuppliesCost As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents lblTotal As Label
+    Friend WithEvents txtTotalCost As TextBox
+    Friend WithEvents txtSubTotal As TextBox
+    Friend WithEvents txtSalesTax As TextBox
+    Friend WithEvents btnCalculate As Button
+End Class
---
+++ Repair Bill/Repair Bill/Form1.vb
@@ -0,0 +1,41 @@
+﻿Public Class frmRepairBill
+
+    Dim customerName As String
+
+    Dim hoursWorked, partsCost, suppliesCost, subTotal, totalCost, salesTax As Double
+
+    Private Sub txtCustomerName_TextChanged(sender As Object, e As EventArgs) Handles txtCustomerName.TextChanged
+
+
+    End Sub
+
+    Const Tax As Double = 0.05
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+
+        If txtCustomerName.Text <> "" And txtHoursWorked.Text <> "" And txtPartsCost.Text <> "" And txtSuppliesCost.Text <> "" Then
+
+            customerName = CStr(txtCustomerName.Text)
+            hoursWorked = CDbl(txtHoursWorked.Text) * 35
+            partsCost = CDbl(txtPartsCost.Text)
+            suppliesCost = CDbl(txtSuppliesCost.Text)
+
+            subTotal = hoursWorked + partsCost + suppliesCost
+
+            salesTax = subTotal * Tax
+
+            totalCost = salesTax + subTotal
+
+            txtSubTotal.Text = subTotal
+            txtSalesTax.Text = salesTax
+            txtTotalCost.Text = totalCost
+
+        Else
+            MessageBox.Show("Please enter all the required information")
+
+        End If
+
+    End Sub
+
+
+End Class
