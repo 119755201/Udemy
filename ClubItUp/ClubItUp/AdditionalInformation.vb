62e4fe618018e1883603945fb2722ba1c8574617
Corey O Donovan <119755201@umail.ucc.ie>
2020-12-14 00:05:34 +0000

    Added more functionality

--- ClubItUp/ClubItUp/AdditionalInformation.vb
+++ ClubItUp/ClubItUp/AdditionalInformation.vb
@@ -22,4 +22,9 @@
         Me.Hide()
 
     End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+
+        Me.Close()
+    End Sub
 End Class
--- ClubItUp/ClubItUp/Module2.vb
+++ ClubItUp/ClubItUp/Module2.vb
@@ -2,7 +2,7 @@
 
     Public EmployeeEmail As String
     Public FullName As String
-
+    Public CustomerName As String
 
 
 End Module
